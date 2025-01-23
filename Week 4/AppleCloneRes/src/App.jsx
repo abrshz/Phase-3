949b48ec5c65e9faf2b8897610d08e504dbf700a
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-01-23 09:14:09 +0300

    Week 4 Apple website by React componenets

---
+++ Week 4/AppleCloneRes/src/App.jsx
@@ -0,0 +1,34 @@
+
+
+import "./css/bootstrap.css"
+import './App.css'
+import AlertSection from './Components/Alert Section/AlertSection'
+import FirstSection from './Components/First Section/FirstSection'
+import Header from './Components/Header/Header'
+import SecondSection from "./Components/Second Section/SecondSection"
+import ThirdSection from "./Components/ThirdSection/ThirdSection"
+import FourthSection from "./Components/Fourth Section/FourthSection"
+import FifthSection from "./Components/Fifth Section/FifthSection"
+import SixthSection from "./Components/Sixth Section/SixthSection"
+import Footer from "./Components/Footer/Footer"
+
+
+function App() {
+  
+
+  return (
+    <>
+<Header />
+<AlertSection />
+<FirstSection />
+<SecondSection />
+<ThirdSection />
+<FourthSection />
+<FifthSection />
+<SixthSection />
+<Footer />
+    </>
+  )
+}
+
+export default App
